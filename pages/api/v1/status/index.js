67a6cc481cb8c0ade89fb378c7880f7c2f27fbc8
ogerenciador <tiagopsgp@outlook.com>
2025-01-03 20:17:53 +0000

    alterado metodo a api para json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
 function status(request, response){
-  response.status(200).send("Alunos do curso.dev são acima da média")
+  response.status(200).json("Alunos do curso.dev são acima da média")
 /*json({"chave":"Alunos do curso.dev são acima da média"})*/
 }
 export default status;

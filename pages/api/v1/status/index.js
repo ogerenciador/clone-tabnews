b452ddf60514d1ee41057f4203fdcb1439565a14
ogerenciador <tiagopsgp@outlook.com>
2025-01-16 15:11:00 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,50 @@
 import database from "infra/database.js";
+
 async function status(request, response) {
-  const result = await database.query("select 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json("Alunos do curso.dev são acima da média");
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  console.log(databaseVersionValue);
+
+  const databaseMaxConnetionResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnetionValue = parseInt(databaseMaxConnetionResult.rows[0].max_connections);
+  console.log(databaseMaxConnetionValue);
+
+/* solução 1 usando length
+  const databaseOpenedConnectionsResult = await database.query("select * from pg_stat_activity where datname = 'local_db';")
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows.length;
+  console.log(databaseOpenedConnectionsResult.rows.length);*/
+/* solução 2 usando count e convertendo na query para inteiro usando ::int
+  const databaseOpenedConnectionsResult = await database.query("select count(*)::int from pg_stat_activity where datname = 'local_db';")
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows.length;
+  console.log(databaseOpenedConnectionsResult.rows[0].count);*/
+/* Solução 3 usando variaveis 
+  const databaseName = "local_db";
+  const databaseOpenedConnectionsResult = await database.query(
+    `select count(*)::int from pg_stat_activity where datname = '${databaseName}';`
+    );
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  console.log(databaseOpenedConnectionsResult.rows[0].count);*/
+  //Query parametrizadas segura de sql injection
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "select count(*)::int from pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  console.log(databaseOpenedConnectionsResult.rows[0].count);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies:{
+      database:{
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnetionValue,
+        opened_conections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
+
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
-  //console.log(response);
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();//que o updated_at esteja definido
+ 
+
+  const parseUpdatedAt =  new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+  
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_conections).toEqual(1);
+  
+  expect(responseBody).not.toHaveProperty('password');
+  expect(responseBody).not.toHaveProperty('email');
 });

c783368879de6cd0670a034c09bf89f2dde761a4
ogerenciador <tiagopsgp@outlook.com>
2025-01-17 13:55:38 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: getSSLValues(),
   });
   
   try{
@@ -20,9 +20,17 @@ async function query(queryObject) {
     await client.end();
   }
   
-  
-  
 }
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  
+  return process.env.NODE_ENV === 'development' ? false : true;
+}
